d73db99055d5d5fbc69868b1fb072d395da350a3
Alviam564 <alvarezwilliam543@gmail.com>
2025-05-18 15:26:21 -0400

    NewItemscompleted

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import { BASE_URL } from "../utils/fetchfromApi";
 import Slick from "../utils/Slick"
 import Skeleton from "../UI/Skeleton";
+import { Link } from "react-router-dom";
 
 
 const HotCollections = () => {
@@ -45,6 +46,7 @@ const HotCollections = () => {
                     <Skeleton width="100%" height="180px" borderRadius="10px" />
                     <div className="nft_coll_pp">
                       <Skeleton width="60px" height="50px" borderRadius="100%" />
+                      <i className="fa fa-check"></i>
                     </div>
                     <div className="nft_coll_info">
                     <Skeleton width="40%" height="20px" borderRadius="4px" />
@@ -57,7 +59,31 @@ const HotCollections = () => {
               ))}
             </div>
           ) : ( 
-            <Slick items={ hotcollection }> </Slick>
+            <Slick>
+              {hotcollection.map((collection, index) => (
+                <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+                  <div className="nft_coll">
+                    <div className="nft_wrap">
+                    <Link to={`/item-details/${collection.nftId}`}>
+                      <img src={collection.nftImage} className="lazy img-fluid" alt="" />
+                    </Link>
+                    </div>
+                    <div className="nft_coll_pp">
+                      <Link to={`/author/${collection.authorId}`}>
+                        <img className="lazy pp-coll" src={collection.authorImage} alt="" />
+                      </Link>
+                      <i className="fa fa-check"></i>
+                    </div>
+                    <div className="nft_coll_info">
+                      <Link to="/explore">
+                        <h4>{collection.title}</h4>
+                      </Link>
+                      <span>{collection.code}</span>
+                    </div>
+                  </div>
+                </div>
+              ))} 
+            </Slick>
           )}
         </div>
       </div>
--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import { BASE_URL } from "../utils/fetchfromApiItems"
+import Slick from "../utils/Slick";
+import Skeleton from "../UI/Skeleton";
+
 
 const NewItems = () => {
+
+  const [newitems, setnewitems] = useState([]);
+  const [isLoading, setLoading] = useState(true)
+  const [now, setNow] = useState(Date.now())
+  
+    useEffect(() => {
+      const fetchData = async () => {
+      try {
+        const response = await fetch(BASE_URL);
+        const data = await response.json();
+        setnewitems(data);     
+        } catch (error) {
+          console.error("Failed to fetch data:", error);
+        } finally {
+          setLoading(false)
+        }
+      }
+  
+      fetchData();
+    }, []);
+
+
+    function countdown(expiryTime, now){
+      const timeLeft = expiryTime - now;
+
+      if (timeLeft <= 0) return "Expired"
+      
+      let hours = Math.floor(timeLeft / (1000 * 60 * 60))
+      let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60))
+      let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000)
+
+      const pad = (n) => n.toString().padStart(2)
+      return `${pad(hours)}h ${pad(minutes)}m ${(seconds)}s`
+    }
+
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setNow(Date.now());
+      }, 1000)
+
+      return () => clearInterval(interval)
+    }, []);
+
   return (
     <section id="section-items" className="no-bottom">
       <div className="container">
@@ -14,62 +59,88 @@ const NewItems = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft__item">
-                <div className="author_list_pp">
-                  <Link
-                    to="/author"
-                    data-bs-toggle="tooltip"
-                    data-bs-placement="top"
-                    title="Creator: Monica Lucas"
-                  >
-                    <img className="lazy" src={AuthorImage} alt="" />
-                    <i className="fa fa-check"></i>
-                  </Link>
+          {isLoading ? (
+            <div className="row">
+              {Array(4).fill().map((_, index) => (
+                <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+                  <div className="nft__item">
+                    <div className="author_list_pp">
+                      <Skeleton width="50px" height="50px" borderRadius="100%" />
+                      <i className="fa fa-check"></i>
+                    </div>
+                    <div className="nft__item_preview">
+                    <Skeleton width="100%" height="280px" />
+                    </div>
+                    <div className="nft__item_info">
+                      <Skeleton width="69%" height="25px" borderRadius="4px" />
+                    </div>
+                    <div className="nft__item_price">
+                      <Skeleton width="35%" height="20px" borderRadius="1px" />
+                    </div>
+                    <div className="nft__item_like">
+                      <Skeleton width="35px" height="20px" borderRadius="1px" />
+                    </div>
+                  </div>
                 </div>
-                <div className="de_countdown">5h 30m 32s</div>
-
-                <div className="nft__item_wrap">
-                  <div className="nft__item_extra">
-                    <div className="nft__item_buttons">
-                      <button>Buy Now</button>
-                      <div className="nft__item_share">
-                        <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-facebook fa-lg"></i>
-                        </a>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-twitter fa-lg"></i>
-                        </a>
-                        <a href="">
-                          <i className="fa fa-envelope fa-lg"></i>
-                        </a>
+              ))}
+            </div>
+          ) : ( 
+            <Slick>
+              {newitems.map((items, index) => (
+                <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+                  <div className="nft__item">
+                    <div className="author_list_pp">
+                      <Link
+                      to={`/author/${items.authorId}`}
+                        data-bs-toggle="tooltip"
+                        data-bs-placement="top"
+                      >
+                        <img className="lazy" src={items.authorImage} alt="authorImage" />
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="de_countdown">{countdown(items.expiryDate, now)}</div>
+                    <div className="nft__item_wrap">
+                      <div className="nft__item_extra">
+                        <div className="nft__item_buttons">
+                          <button>Buy Now</button>
+                          <div className="nft__item_share">
+                            <h4>Share</h4>
+                            <a href="" target="_blank" rel="noreferrer">
+                              <i className="fa fa-facebook fa-lg"></i>
+                            </a>
+                            <a href="" target="_blank" rel="noreferrer">
+                              <i className="fa fa-twitter fa-lg"></i>
+                            </a>
+                            <a href="">
+                              <i className="fa fa-envelope fa-lg"></i>
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                      <Link to={`/item-details/${items.nftId}`}>
+                        <img
+                          src={items.nftImage}
+                          className="lazy nft__item_preview"
+                          alt="nft__item_preview"
+                        />
+                      </Link>
+                    </div>
+                    <div className="nft__item_info">
+                      <Link to={`/item-details/${items.nftId}`}>
+                        <h4>{items.title}</h4>
+                      </Link>
+                      <div className="nft__item_price">{items.price} ETH</div>
+                      <div className="nft__item_like">
+                        <i className="fa fa-heart"></i>
+                        <span>{items.likes}</span>
                       </div>
                     </div>
                   </div>
-
-                  <Link to="/item-details">
-                    <img
-                      src={nftImage}
-                      className="lazy nft__item_preview"
-                      alt=""
-                    />
-                  </Link>
                 </div>
-                <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <div className="nft__item_price">3.08 ETH</div>
-                  <div className="nft__item_like">
-                    <i className="fa fa-heart"></i>
-                    <span>69</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
+              ))}
+            </Slick>
+          )}
         </div>
       </div>
     </section>
