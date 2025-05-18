f95627bf057e118b1ad43575106a3637677fce48
Alviam564 <alvarezwilliam543@gmail.com>
2025-05-18 11:19:24 -0400

    betterChanges

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import { BASE_URL } from "../utils/fetchfromApi";
 import Slick from "../utils/Slick"
 import Skeleton from "../UI/Skeleton";
+import { Link } from "react-router-dom";
 
 
 const HotCollections = () => {
@@ -57,7 +58,31 @@ const HotCollections = () => {
               ))}
             </div>
           ) : ( 
-            <Slick items={ hotcollection }> </Slick>
+            <Slick>
+              {hotcollection.map((collection, index) => (
+                <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+                  <div className="nft_coll">
+                    <div className="nft_wrap">
+                    <Link to="/item-details">
+                      <img src={collection.nftImage} className="lazy img-fluid" alt="" />
+                    </Link>
+                    </div>
+                    <div className="nft_coll_pp">
+                      <Link to="/author">
+                        <img className="lazy pp-coll" src={collection.authorImage} alt="" />
+                      </Link>
+                      <i className="fa fa-check"></i>
+                    </div>
+                    <div className="nft_coll_info">
+                      <Link to="/explore">
+                        <h4>{collection.title}</h4>
+                      </Link>
+                      <span>{collection.code}</span>
+                    </div>
+                  </div>
+                </div>
+              ))} 
+            </Slick>
           )}
         </div>
       </div>
